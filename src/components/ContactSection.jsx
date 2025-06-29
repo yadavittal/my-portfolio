8d38d73284651750189bb255eef4381954a019cb
yadavittal <vittalyadav8328@gmail.com>
2025-06-29 14:07:28 +0530

    small changes in contactSection

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -130,7 +130,7 @@ export const ContactSection = () => {
                   name="name"
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-                  placeholder="Pedro Machado..."
+                  placeholder="Sandy Gayakwad..."
                 />
               </div>
 
@@ -148,7 +148,7 @@ export const ContactSection = () => {
                   name="email"
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-                  placeholder="john@gmail.com"
+                  placeholder="yadav@gmail.com"
                 />
               </div>
 
